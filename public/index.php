dbab5e704b9071a3a33984427a22459b217e240c
stinkymonkeyph <monkeydev.team@gmail.com>
2018-03-27 18:24:54 -0700

    comments

--- public/index.php
+++ public/index.php
@@ -12,17 +12,16 @@ require __DIR__.'/../core/autoloader.php';
 **/
 use Core\Helper;
 
-/**
-	Handle Request
-**/
+
+// Handle Request
+
 use Core\Router;
 
 Router::parse_route($_SERVER['REQUEST_URI']); //parse http request
 Router::load_routes(); //load user defined routes
+ 
+// Render Template
 
-/** 
-	Render Template
-**/
 Core\View::render_template();
 
 
