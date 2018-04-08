4fc6598c717131cb3bd2c9d9866e770be755322f
stinkymonkeyph <monkeydev.team@gmail.com>
2018-04-08 01:46:37 -0700

    added some

---
+++ core/Encryption.php
@@ -0,0 +1,9 @@
+<?php 
+
+class Encryption
+{
+	// encryption class
+}
+
+
+?>
---
+++ views/form.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Test Form</title>
+</head>
+<body>
+	<form action="/public/test/form" method="POST">
+		<input type="tex" name="first name"/>
+		<input type="text" name="second name"/>
+		<input type="hidden" name="csrf_token" value="<?php Helper::csrf_token() ?>"/>
+		<input type="submit"/>
+	</form>
+</body>
+</html>
